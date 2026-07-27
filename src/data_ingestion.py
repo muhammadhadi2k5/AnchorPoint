04278cc748fa34d4a712fa9c58d4fbc5593bfe34
muhammadhadi2k5 <muhammadhadi.2k5@gmail.com>
2026-07-27 16:42:29 +0500

    add: VectorDB to store embeddings

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -1,4 +1,6 @@
 import os
+import sys
+sys.stdout.reconfigure(encoding="utf-8")
 from langchain_community.document_loaders import PyPDFLoader, PyMuPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from pathlib import Path
@@ -52,11 +54,11 @@ def chunking(documents, chunk_size=1000, chunk_overlap=200):
     split_docs = text_splitter.split_documents(documents)
     print(f"Split {len(documents)} documents into {len(split_docs)} chunks")
 
-    print(f"\nExample Chunk")
-    print(f"Content: {split_docs[0].page_content[:200]}")
-    print(f"MetaData: {split_docs[0].metadata}")
+    # print(f"\nExample Chunk")
+    # print(f"Content: {split_docs[0].page_content[:200]}")
+    # print(f"MetaData: {split_docs[0].metadata}")
 
-    return chunking
+    return split_docs
 
 all_pdf_documents = process_all_pdfs("data\pdf")
 chunks = chunking(all_pdf_documents)
@@ -72,7 +74,7 @@ class EmbeddingManager:
         try:
             print(f"\n\nLoading embedding model: {self.model_name}\n")
             self.model = SentenceTransformer(self.model_name)
-            print(f"\n\nModel successfully loaded. Embedding dimensions: {self.model.get_embedding_dimension()}\n")
+            print(f"\n\nModel successfully loaded. Embedding dimensions: {self.model.get_sentence_embedding_dimension()}\n")
         except Exception as e:
             print(f"Error loading embedding model: {e}")
             raise
@@ -86,9 +88,78 @@ class EmbeddingManager:
         print(f"Generated embeddings with shape: {embeddings.shape}")
         return embeddings
 
-    def get_embedding_dimension(self) -> int:
-        if not self.model:
-            raise ValueError("Embedding model is not loaded.")
-        return self.model.get_sentence_embedding_dimension()
 
 embedding_manager = EmbeddingManager()
+
+class VectorDB:
+
+    def __init__(self, collection_name: str = "pdf_chunks", persist_directory: str = "data/chroma"):
+        self.collection_name = collection_name
+        self.persist_directory = persist_directory
+        self.client = None
+        self.collection = None
+        self._initialize_chromadb()
+
+    def _initialize_chromadb(self):
+        try:
+            os.makedirs(self.persist_directory, exist_ok=True)
+            self.client = chromadb.PersistentClient(path=self.persist_directory)
+
+            self.collection = self.client.get_or_create_collection(
+                name=self.collection_name,
+                metadata={"description": "Collection of PDF chunks with embeddings for RAG"})
+
+            print(f"VectorDB initialized with collection: {self.collection_name} at {self.persist_directory}")
+            print(f"Existing documents in collection: {self.collection.count()}")
+
+        except Exception as e:
+            print(f"Error initializing vectorDB: {e}")
+            raise
+
+    def add_documents(self, documents:List[Any], embeddings: np.ndarray):
+        if len(documents) != len(embeddings):
+            raise ValueError("Number of documents and embeddings must match.")
+
+        print(f"Adding {len(documents)} documents to the vectorDB...")
+
+        ids = []
+        metadatas=[]
+        documents_text=[]
+        embeddings_list=[]
+
+        for i, (doc, embedding) in enumerate(zip(documents, embeddings)):
+            #make unique ids
+            doc_id = f"doc_{uuid.uuid4().hex[:8]}_{i}"
+            ids.append(doc_id)
+
+            #make some metadata
+            metadata = dict(doc.metadata)
+            metadata['doc_index'] = i
+            metadata['content_length'] = len(doc.page_content)
+            metadatas.append(metadata)
+
+            #add doc content in list
+            documents_text.append(doc.page_content)
+
+            #add embeddings in list
+            embeddings_list.append(embeddings.tolist())
+
+        try:
+            self.collection.add(
+                ids=ids,
+                metadatas=metadatas,
+                documents=documents_text,
+                embeddings=embeddings_list
+            )
+            print(f"Successfully added {len(documents)} documents to the vectorDB.")
+            print(f"Total documents in collection: {self.collection.count()}")
+        except Exception as e:
+            print(f"Error adding documents to vectorDB: {e}")
+            raise
+
+vectorDB=VectorDB()
+print(vectorDB)
+
+#convert the text to embeddings
+texts = [doc.page_content for doc in chunks]
+print(texts)
